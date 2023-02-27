81c63d0e8ba0ded95b05ec5a27bcfa919eb3d6af
MCierny <122530624+MCierny@users.noreply.github.com>
2023-02-27 15:33:04 +0100

    Add reset, eraser, rainbow buttons with functionality

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 const btnGrid = document.getElementById('btn-grid');
+const btnReset = document.getElementById('btn-reset');
+const btnErase = document.getElementById('btn-erase');
+const btnRandom = document.getElementById('btn-random');
+const btnColor = document.getElementById('btn-color');
 const gridContainer = document.querySelector('.grid-container');
-const randomColor = Math.floor(Math.random()*16777215).toString(16);
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 
 let gridSize;
@@ -19,6 +25,9 @@ function drawGrid() {
             let gridDiv = document.createElement('div');
             gridDiv.classList.add('grid-div');
             gridRow.appendChild(gridDiv);
+            // gridDiv.addEventListener('mousemove', function (event) {
+            // gridDiv.classList.add('grid-after');
+            // });
         }
     }
 }
@@ -33,3 +42,68 @@ btnGrid.addEventListener('click', () => {
     getGrid();
     drawGrid();
 });
+
+btnReset.addEventListener('click', () => {
+    clearGrid();
+    drawGrid();
+});
+
+btnErase.onclick = () => {
+    document.onmouseover = function(e){
+        const target = e.target.closest(".grid-div");
+        if (target) {
+            target.style.backgroundColor = 'white';        
+        }
+      };
+};
+
+btnRandom.onclick = () => { 
+    document.onmouseover = function(e){
+        const target = e.target.closest(".grid-div");
+        const randomColor = '#'+(Math.floor(Math.random()*16777215).toString(16));
+        if (target) {
+            target.style.backgroundColor = randomColor;
+        }
+      };
+};
+
+
+// btnErase.onclick = () => {
+//     document.onmousemove = function(e){
+//         const target = e.target.closest(".grid-div");
+//         if (target) {
+//             target.classList.remove('grid-after-black')
+//             target.classList.add('grid-after-white');
+//         }
+//       };
+// };
+
+// btnRandom.onclick = () => {
+//     document.onmousemove = function(e){
+//         const target = e.target.closest(".grid-div");
+//         if (target) {
+//             target.classList.remove('grid-after-white')
+//             target.classList.add('grid-after-black');
+//         }
+//       };
+// };
+
+// btnErase.addEventListener('click', () => {
+//     document.addEventListener("mousemove", function(e){
+//         const target = e.target.closest(".grid-div");
+//         if (target) {
+//             target.classList.add('grid-after-white');
+//         }
+//       });
+// });
+
+// btnRandom.addEventListener('click', () => {
+//     document.addEventListener('mousemove', function(e){
+//         const target = e.target.closest(".grid-div");
+//         if (target) {
+//             target.classList.add('grid-after-black');
+//         }
+//       });
+// });
+
+
