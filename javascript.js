23a17de5340c75fb518547274785853749dc9fa1
MCierny <122530624+MCierny@users.noreply.github.com>
2023-02-28 13:33:04 +0100

    Add new functionalities and basic UI
    New functionalities added: slider to select grid size and apply with
    button, button to toggle grid lines, reset button to clear the grid of
    all drawings but retain the grid size, eraser button to clear desired
    grids, rainbow button to draw each grid with random color, color picker
    button to choose with which color the user wants to draw.
    
    Added click and hold function -  start drawing on mousedown, keep drawing
    on hold,stop drawing on mouseup
    
    Added header, footer, left controls panel and centered the grid canvas

--- javascript.js
+++ javascript.js
@@ -1,36 +1,48 @@
 const btnGrid = document.getElementById('btn-grid');
+const btnToggle = document.getElementById('toggle');
 const btnReset = document.getElementById('btn-reset');
 const btnErase = document.getElementById('btn-erase');
 const btnRandom = document.getElementById('btn-random');
 const btnColor = document.getElementById('btn-color');
 const gridContainer = document.querySelector('.grid-container');
+const sliderSize = document.querySelector('.slider-size');
+let slider = document.querySelector('input');
 
+
+// Click and Hold mouse event (for optionals)
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-
+// Get GRID size by prompt.
 let gridSize;
 function getGrid() {
     gridSize = Number(prompt('What is your desired grid size?',''));
 }
 
+// Get GRID size by slider input, print slider value to HTML DOM.   
+let sliderOutput = 32;
+slider.oninput = function() {
+    sliderOutput = this.value;
+    sliderSize.textContent = `${this.value} x ${this.value}`;
+  }
+
+drawGrid();
 function drawGrid() {
-    for (let j = 1; j <= gridSize; j++) {
+    for (let j = 1; j <= sliderOutput; j++) { // Change sliderOutput to gridSize if I want to get grid size input by prompt
         let gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
         gridContainer.appendChild(gridRow);
 
-        for (let i = 1; i <= gridSize; i++) {
+        for (let i = 1; i <= sliderOutput; i++) {
             let gridDiv = document.createElement('div');
             gridDiv.classList.add('grid-div');
+            gridDiv.classList.add('toggle');
             gridRow.appendChild(gridDiv);
-            // gridDiv.addEventListener('mousemove', function (event) {
-            // gridDiv.classList.add('grid-after');
-            // });
         }
     }
 }
+
 
 function clearGrid() {
     const myNode = document.getElementById("grid-container");
@@ -39,9 +51,16 @@ function clearGrid() {
 
 btnGrid.addEventListener('click', () => {
     clearGrid();
-    getGrid();
+    // getGrid(); // If PROMPT is used.
     drawGrid();
 });
+
+btnToggle.onclick = () => {
+    let gridDiv = document.querySelectorAll('.grid-div');
+    for (let i = 0; i < gridDiv.length; i++) {
+        gridDiv[i].classList.toggle('toggle')
+    }
+};
 
 btnReset.addEventListener('click', () => {
     clearGrid();
@@ -49,9 +68,9 @@ btnReset.addEventListener('click', () => {
 });
 
 btnErase.onclick = () => {
-    document.onmouseover = function(e){
+    document.onmouseover = function(e){ // using Event Delegation to target newly created DOMs
         const target = e.target.closest(".grid-div");
-        if (target) {
+        if (target && mouseDown) {
             target.style.backgroundColor = 'white';        
         }
       };
@@ -60,50 +79,29 @@ btnErase.onclick = () => {
 btnRandom.onclick = () => { 
     document.onmouseover = function(e){
         const target = e.target.closest(".grid-div");
-        const randomColor = '#'+(Math.floor(Math.random()*16777215).toString(16));
-        if (target) {
+        const randomColor = '#'+(Math.floor(Math.random()*16777215).toString(16)); // Generate RANDOM color, must be inside function to generate a new random color on each mouseover.
+        if (target && mouseDown) {
             target.style.backgroundColor = randomColor;
         }
       };
 };
 
+function update(picker) {
+    document.onmouseover = function(e){
+        const target = e.target.closest(".grid-div");
+        if (target && mouseDown) {
+            target.style.backgroundColor = picker.toHEXString();
+        }
+      };
+}
 
-// btnErase.onclick = () => {
-//     document.onmousemove = function(e){
-//         const target = e.target.closest(".grid-div");
-//         if (target) {
-//             target.classList.remove('grid-after-black')
-//             target.classList.add('grid-after-white');
-//         }
-//       };
-// };
-
-// btnRandom.onclick = () => {
-//     document.onmousemove = function(e){
-//         const target = e.target.closest(".grid-div");
-//         if (target) {
-//             target.classList.remove('grid-after-white')
-//             target.classList.add('grid-after-black');
-//         }
-//       };
-// };
-
-// btnErase.addEventListener('click', () => {
-//     document.addEventListener("mousemove", function(e){
-//         const target = e.target.closest(".grid-div");
-//         if (target) {
-//             target.classList.add('grid-after-white');
-//         }
-//       });
-// });
-
-// btnRandom.addEventListener('click', () => {
-//     document.addEventListener('mousemove', function(e){
-//         const target = e.target.closest(".grid-div");
-//         if (target) {
-//             target.classList.add('grid-after-black');
-//         }
-//       });
-// });
+gridContainer.onclick = () => {
+    gridContainer.onmouseover = function(e){ // using Event Delegation to target newly created DOMs
+        const target = e.target.closest(".grid-div");
+        if (target && mouseDown) {
+            target.style.backgroundColor = '#CCFFAA';        
+        }
+      };
+};
 
 
